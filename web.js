e43585bdccb62f09bc3bce44aa768109abc9ee7a
Your Name <you@example.com>
2016-10-23 06:42:57 -0400

    .

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var morgan = require('morgan');
+var path= require('path');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/www'));
+// app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+// console.log('/scripts mapped to [' + path.join(__dirname, '/app/scripts') + ']');
+// app.use('/scripts', express.static(path.join(__dirname, '/app/scripts')));
+app.listen(process.env.PORT || 5000);
+app.use(function(req, res){
+res.sendFile(__dirname + "/www/index.html");
+});
