2bb2e6cc2bb69e528e3ab97a1530ac09dd0eca16
Your Name <you@example.com>
2016-10-23 07:01:22 -0400

    .

--- web.js
+++ web.js
@@ -7,8 +7,9 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/www'));
 // app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 // console.log('/scripts mapped to [' + path.join(__dirname, '/app/scripts') + ']');
-// app.use('/scripts', express.static(path.join(__dirname, '/app/scripts')));
+app.use('/build', express.static(path.join(__dirname, '/www/build')));
 app.listen(process.env.PORT || 5000);
 app.use(function(req, res){
 res.sendFile(__dirname + "/www/index.html");
 });
+console.log('1 app started!');
